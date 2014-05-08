6c16e0130a8e66d823b1754160036fa8e9342027
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-08 23:33:38 +0300

    no aftakia

--- windoze-fun/box_add.bat
+++ windoze-fun/box_add.bat
@@ -1,7 +1,7 @@
 REM DRIVE="e"
 REM set -x
 REM export PATH=$PATH:"/cygdrive/e/Program Files/Oracle/VirtualBox/"
-SET VAGRANT_LOG="debug"
+SET VAGRANT_LOG=debug
 REM export VAGRANT_HOME="/cygdrive/e/home/${USER}/vagrant_home"
 REM export VAGRANT_DOTFILE_PATH="/cygdrive/e/home/${USER}/vagrant_dotfile_path"
 vagrant box add --clean hashicorp/precise64  --provider virtualbox --insecure
