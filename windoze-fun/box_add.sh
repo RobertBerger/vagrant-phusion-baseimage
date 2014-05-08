e363c53ffc96bd417165181177684feb27d123cb
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-08 22:59:01 +0300

    mumu

--- windoze-fun/box_add.sh
+++ windoze-fun/box_add.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DRIVE="e"
 set -x
+export PATH=$PATH:"/cygdrive/e/Program Files/Oracle/VirtualBox/"
 export VAGRANT_LOG="debug"
 export VAGRANT_HOME="/cygdrive/e/home/${USER}/vagrant_home"
 export VAGRANT_DOTFILE_PATH="/cygdrive/e/home/${USER}/vagrant_dotfile_path"
