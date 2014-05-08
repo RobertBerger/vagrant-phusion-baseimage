3e938239663749dbbfaa734774107ed9051fb00a
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-08 22:15:39 +0300

    more fun

--- windoze-fun/box_add.sh
+++ windoze-fun/box_add.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -x
 export VAGRANT_LOG="debug"
+export VAGRANT_HOME="/home/User/VAGRANT_HOME"
+export VAGRANT_DOTFILE_PATH="/home/User/VAGRANT_DOTFILE_PATH"
 vagrant box add --clean hashicorp/precise64  --provider virtualbox --insecure
