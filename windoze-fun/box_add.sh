d090d1e4284aeecf7e2a33d76ae50b146dd45ef1
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-08 22:24:08 +0300

    mala

--- windoze-fun/box_add.sh
+++ windoze-fun/box_add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 export VAGRANT_LOG="debug"
-export VAGRANT_HOME="/home/User/VAGRANT_HOME"
-export VAGRANT_DOTFILE_PATH="/home/User/VAGRANT_DOTFILE_PATH"
+export VAGRANT_HOME="/home/${USER}/vagrant_home"
+export VAGRANT_DOTFILE_PATH="/home/${USER}/vagrant_dotfile_path"
 vagrant box add --clean hashicorp/precise64  --provider virtualbox --insecure
---
+++ windoze-fun/log_box_add.sh
@@ -0,0 +1,1 @@
+(./box_add.sh >> box-add.txt ) 2>&1 | tee -a box-add.txt
