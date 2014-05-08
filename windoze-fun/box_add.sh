9464663815e7a6f59f34c02fd75bfbdfd15119fc
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-08 22:08:02 +0300

    mala

--- windoze-fun/box_add.sh
+++ windoze-fun/box_add.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 export VAGRANT_LOG="debug"
-vagrant box add vagrant box add --clean hashicorp/precise64  --provider virtualbox --insecure
+vagrant box add --clean hashicorp/precise64  --provider virtualbox --insecure
